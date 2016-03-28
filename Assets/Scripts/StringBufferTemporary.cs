2cd544f74d6c0ca13288320049d55d4956d70381
wotakuro <wotakuro@gmail.com>
2016-03-29 02:34:10 +0900

    commit StringBuilderTemporary

---
+++ Assets/Scripts/StringBufferTemporary.cs
@@ -0,0 +1,119 @@
+﻿using System.Threading;
+using System.Text;
+
+namespace StringBufferTemporary
+{
+	/// <summary>
+	/// 文字列連結周りの処理を、まともにするコードを簡単に書けるためのクラス
+	/// string str = "aaa" + 20 + "bbbb"; のようになっているところを…
+	/// string str = Sbt.i + "aaa" + 20 + "bbbb"; とすることで、処理が大分まともになります。
+	/// 
+	/// Sbt.iは、ThreadSafeではありません。あと同じオブジェクトを使いまわします。
+	/// そういうケースで使いたい場合は、Sbt.Create()を代わりに使用してください
+	/// </summary>
+	public class Sbt
+	{
+		private static Sbt s_this;
+		private StringBuilder sb;
+
+		static Sbt()
+		{
+			s_this = new Sbt(1024);
+		}
+		private Sbt(int capacity)
+		{
+			sb = new StringBuilder(capacity);
+		}
+
+		public static Sbt Create(int capacity)
+		{
+			return new Sbt(capacity);
+		}
+
+		public static Sbt i
+		{
+			get
+			{
+				s_this.sb.Length = 0;
+				return s_this;
+			}
+		}
+		public int Capacity
+		{
+			set { this.sb.Capacity = value; }
+			get { return sb.Capacity; }
+		}
+
+		public int Length
+		{
+			set { this.sb.Length = value; }
+			get { return this.sb.Length; }
+		}
+		public void Remove(int startIndex, int length)
+		{
+			sb.Remove(startIndex, length);
+		}
+		public void Replace(string oldValue, string newValue)
+		{
+			sb.Replace(oldValue, newValue);
+		}
+
+		public override string ToString()
+		{
+			return sb.ToString();
+		}
+
+
+		public static implicit operator string(Sbt t)
+		{
+			return t.ToString();
+		}
+
+		#region ADD_OPERATOR
+		public static Sbt operator +(Sbt t, bool v)
+		{
+			t.sb.Append(v);
+			return t;
+		}
+		public static Sbt operator +(Sbt t, int v)
+		{
+			t.sb.Append(v);
+			return t;
+		}
+		public static Sbt operator +(Sbt t,short v){
+			t.sb.Append(v);
+			return t;
+		}
+		public static Sbt operator +(Sbt t, byte v)
+		{
+			t.sb.Append(v);
+			return t;
+		}
+		public static Sbt operator +(Sbt t, float v)
+		{
+			t.sb.Append(v);
+			return t;
+		}
+		public static Sbt operator +(Sbt t, char c)
+		{
+			t.sb.Append(c);
+			return t;
+		}
+		public static Sbt operator +(Sbt t, char[] c)
+		{
+			t.sb.Append(c);
+			return t;
+		}
+		public static Sbt operator +(Sbt t, string str)
+		{
+			t.sb.Append(str);
+			return t;
+		}
+		public static Sbt operator +(Sbt t, StringBuilder sb)
+		{
+			t.sb.Append(sb);
+			return t;
+		}
+		#endregion ADD_OPERATOR
+	}
+}
---
+++ Assets/Test/TestScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using StringBufferTemporary;
+
+public class TestScript : MonoBehaviour {
+
+	bool sbtFlag = false;
+	TextMesh txt;
+	// Use this for initialization
+	void Start () {
+		txt = this.GetComponent<TextMesh>();
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			sbtFlag = !sbtFlag;
+		}
+		if (sbtFlag)
+		{
+			UpdateSbtOn();
+		}
+		else
+		{
+			UpdateSbtOff();
+		}
+		int gcUsed = (int)Profiler.GetMonoUsedSize();
+		this.txt.text = Sbt.i + "sbtFlag " + sbtFlag + "\n" + "detltaTime:" + Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void UpdateSbtOn()
+	{
+		// Sbt str = Sbt.i + "test";
+		var str = Sbt.i + "test";
+		for (int i = 0; i < 20000; ++i)
+		{
+			str += "a";
+		}
+	}
+	void UpdateSbtOff()
+	{
+		// string str = "test";
+		var str = "test";
+		for (int i = 0; i < 20000; ++i)
+		{
+			str += "a";
+		}
+	}
+}
